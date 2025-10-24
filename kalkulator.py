45a39db3147da85463f6bc4cfe714b85d3b31bae
Seweryn Mlodawski <seweryn.mlodawski@gmail.com>
2025-10-24 04:20:59 +0200

    Add level of logging

--- kalkulator.py
+++ kalkulator.py
@@ -1,3 +1,9 @@
 import sys                                  # Importuje moduł sys do obsługi argumentów wiersza poleceń
 import logging                              # Importuje moduł logging do obsługi logowania komunikatów
 logging.basicConfig(level=logging.DEBUG)    # Ustawia poziom logowania na DEBUG
+print(f"""Podaj działanie które chcesz wykonać:
+1 - dodawanie
+2 - odejmowanie
+3 - mnożenie
+4 - dzielenie""")
+operation = input("Wpisz numer działania (1/2/3/4): ")  # Pobiera od użytkownika numer działania
