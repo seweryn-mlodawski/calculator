66e62b5722c1e0026ef0cf41c1f4db85ef93682c
Seweryn Mlodawski <seweryn.mlodawski@gmail.com>
2025-10-24 04:28:11 +0200

    Change log method

--- kalkulator.py
+++ kalkulator.py
@@ -1,6 +1,7 @@
 import sys                                  # Importuje moduł sys do obsługi argumentów wiersza poleceń
 import logging                              # Importuje moduł logging do obsługi logowania komunikatów
-logging.basicConfig(level=logging.DEBUG)    # Ustawia poziom logowania na DEBUG
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', filename="logfile.log")    # Ustawia poziom logowania na INFO i logowanie do pliku logfile.log
+#logging.basicConfig(level=logging.DEBUG)    # Ustawia poziom logowania na DEBUG
 print(f"""Podaj działanie które chcesz wykonać:
 1 - dodawanie
 2 - odejmowanie
