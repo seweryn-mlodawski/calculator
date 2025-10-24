94b9be050e0b338c0db7cfd94650470391279a30
Seweryn Mlodawski <seweryn.mlodawski@gmail.com>
2025-10-24 04:06:27 +0200

    Add file kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,3 @@
+import sys                                  # Importuje moduł sys do obsługi argumentów wiersza poleceń
+import logging                              # Importuje moduł logging do obsługi logowania komunikatów
+logging.basicConfig(level=logging.DEBUG)    # Ustawia poziom logowania na DEBUG
